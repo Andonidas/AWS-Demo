90043750af9e7c255e4abe3e5f900e11dc888ec1
Andonidas <152391221@qq.com>
2023-03-14 18:45:42 -0700

    add storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -29,7 +29,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://qevmpd5bhrf47dgxwvv4iqudjq.appsync-api.us-west-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-zbucoo6ygrb65ekcuuru2vsiui"
+    "aws_appsync_apiKey": "da2-zbucoo6ygrb65ekcuuru2vsiui",
+    "aws_user_files_s3_bucket": "awsdemo1231547-staging",
+    "aws_user_files_s3_bucket_region": "us-west-1"
 };
 
 
